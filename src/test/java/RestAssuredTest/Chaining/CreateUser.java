1e68407401b8ce481c12bc47bde068752c866a39
Murali <123637911+muralisiva8896@users.noreply.github.com>
2023-07-03 23:25:31 +0530

    chaining concepts added with testng xml

---
+++ src/test/java/RestAssuredTest/Chaining/CreateUser.java
@@ -0,0 +1,34 @@
+package RestAssuredTest.Chaining;
+
+import com.github.javafaker.Faker;
+import org.json.JSONObject;
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class CreateUser{
+    @Test
+    public void test_createUser(ITestContext context){
+    Faker faker = new Faker();
+    JSONObject data = new JSONObject();
+    String bearerToken = "2d060e66d3c82e26bf94a516ccb0d10c5db9fbfef86573b9d789d06021ac1781";
+
+    data.put("name",faker.name().fullName());
+    data.put("gender", "Male");
+    data.put("email", faker.internet().emailAddress());
+    data.put("status","Inactive");
+
+    int id =given()
+            .header("Authorization","Bearer "+bearerToken)
+            .contentType("application/json")
+            .body(data.toString())
+            .when()
+            .post("https://gorest.co.in/public/v2/users")
+            .jsonPath().getInt("id");
+
+    System.out.println(id);
+    context.setAttribute("user_id", id);
+
+    }
+}
---
+++ src/test/java/RestAssuredTest/Chaining/GetUser.java
@@ -0,0 +1,24 @@
+package RestAssuredTest.Chaining;
+
+import org.testng.ITestContext;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class GetUser {
+
+    @Test
+    public void test_Getuser(ITestContext context){ //context capture this test method attributes
+
+        String bearerToken = "2d060e66d3c82e26bf94a516ccb0d10c5db9fbfef86573b9d789d06021ac1781";
+        int id = (int) context.getAttribute("user_id");//this should come from create user
+        given()
+                .header("Authorization","Bearer "+bearerToken)
+                .pathParam("id",id)
+                .when()
+                .get("https://gorest.co.in/public/v2/users/{id}")
+                .then()
+                .statusCode(200)
+                .log().all();
+    }
+}
